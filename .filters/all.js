a25d71a9375d9b282a14814cde29dcd77f84b008
jonaslagoni <jonas-lt@live.dk>
2020-03-28 18:58:47 +0100

    Added missing dependency for all filter

--- .filters/all.js
+++ .filters/all.js
@@ -1,157 +1,173 @@
 const URL = require('url');
 const path = require('path');
+const _ = require('lodash');
 
-module.exports = ({ Nunjucks, _ }) => {
-  Nunjucks.addFilter('kebabCase', string => {
-    return _.kebabCase(string);
-  });
+module.exports = ({ Nunjucks }) => {
+	Nunjucks.addFilter('kebabCase', string => {
+		return _.kebabCase(string);
+	});
 
-  Nunjucks.addFilter('camelCase', string => {
-    return camelCase(string);
-  });
-  function camelCase(string) {
-    return _.camelCase(string);
-  }
+	Nunjucks.addFilter('camelCase', string => {
+		return camelCase(string);
+	});
+	function camelCase(string) {
+		return _.camelCase(string);
+	}
 
-  Nunjucks.addFilter('firstLowerCase', string => {
-    return _.lowerFirst(string);
-  });
+	Nunjucks.addFilter('firstLowerCase', string => {
+		return _.lowerFirst(string);
+	});
 
-  Nunjucks.addFilter('firstUpperCase', string => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  });
+	Nunjucks.addFilter('firstUpperCase', string => {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	});
 
+	Nunjucks.addFilter('pascalCase', string => {
+		return pascalCase(string);
+	});
+	function pascalCase(string) {
+		string = _.camelCase(string);
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
 
+	Nunjucks.addFilter('toTsType', jsonSchemaType => {
+		switch (jsonSchemaType.toLowerCase()) {
+			case 'string':
+				return 'String';
+			case 'integer':
+			case 'number':
+				return 'Number';
+			case 'boolean':
+				return 'Boolean';
+		}
+	});
 
-  Nunjucks.addFilter('pascalCase', string => {
-    return pascalCase(string);
-  });
-  function pascalCase(string) {
-    string = _.camelCase(string);
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+	Nunjucks.addFilter('constructorParameters', schema => {
+		let returnString = '';
+		if (schema.allOf()) {
+			schema.allOf().forEach(element => {
+				returnString += `${camelCase(element.uid())}: ${pascalCase(
+					element.uid()
+				)}Schema,`;
+			});
+		} else if (schema.oneOf()) {
+			returnString += `oneOf: ${getTypeFromOneOf(schema.oneOf())},`;
+		} else if (schema.anyOf()) {
+			schema.anyOf().forEach(element => {
+				returnString += `${camelCase(element.uid())}: ${pascalCase(
+					element.uid()
+				)}Schema,`;
+			});
+		} else if (schema.uid()) {
+			returnString += `${camelCase(schema.uid())}: ${pascalCase(
+				schema.uid()
+			)}Schema,`;
+		}
+		if (returnString.length > 1) {
+			returnString = returnString.slice(0, -1);
+		}
+		return returnString;
+	});
 
-  Nunjucks.addFilter('toTsType', jsonSchemaType => {
-    switch (jsonSchemaType.toLowerCase()) {
-      case 'string':
-        return 'String';
-      case 'integer':
-      case 'number':
-        return 'Number';
-      case 'boolean':
-        return 'Boolean';
-    }
-  });
+	Nunjucks.addFilter('schemaConstructor', properties => {
+		let returnString = '';
+		for (const [key, value] of Object.entries(properties)) {
+			returnString += `${key},`;
+		}
+		if (returnString.length > 1) {
+			returnString = returnString.slice(0, -1);
+		}
+		return returnString;
+	});
 
-  Nunjucks.addFilter('constructorParameters', schema => {
-    let returnString = '';
-    if (schema.allOf()) {
-      schema.allOf().forEach(element => {
-        returnString += `${camelCase(element.uid())}: ${pascalCase(element.uid())}Schema,`;
-      });
-    } else if (schema.oneOf()) {
-      returnString += `oneOf: ${getTypeFromOneOf(schema.oneOf())},`;
-    } else if (schema.anyOf()) {
-      schema.anyOf().forEach(element => {
-        returnString += `${camelCase(element.uid())}: ${pascalCase(element.uid())}Schema,`;
-      });
-    } else if (schema.uid()) {
-      returnString += `${camelCase(schema.uid())}: ${pascalCase(schema.uid())}Schema,`;
-    }
-    if (returnString.length > 1) {
-      returnString = returnString.slice(0, -1);
-    }
-    return returnString;
-  });
+	Nunjucks.addFilter('oneLine', string => {
+		if (!string) return string;
+		return string.replace(/\n/g, ' ');
+	});
 
-  Nunjucks.addFilter('schemaConstructor', properties => {
-    let returnString = '';
-    for (const [key, value] of Object.entries(properties)) {
-      returnString += `${key},`;
-    }
-    if (returnString.length > 1) {
-      returnString = returnString.slice(0, -1);
-    }
-    return returnString;
-  });
+	function getTypeFromOneOf(oneFromSchema) {
+		let type = '';
 
-  Nunjucks.addFilter('oneLine', string => {
-    if (!string) return string;
-    return string.replace(/\n/g, ' ');
-  });
+		if (oneFromSchema.oneOf().length > 0) {
+			type += `${getTypeFromOneOf(oneFromSchema.oneOf())}Schema`;
+		}
 
-  function getTypeFromOneOf(oneFromSchema) {
-    let type = '';
+		for (var i = 0; i < oneOfSchema.length; i++) {
+			let schema = oneOfSchema[i];
+			if (type !== '') {
+				type += '|';
+			}
+			if (oneOfSchema.length == i + 1) {
+				type += schema.uid();
+			} else {
+				type += schema.uid() + '|';
+			}
+		}
+		return type;
+	}
+	Nunjucks.addFilter('oneOfSchemaType', getTypeFromOneOf);
 
-    if (oneFromSchema.oneOf().length > 0) {
-      type += `${getTypeFromOneOf(oneFromSchema.oneOf())}Schema`;
-    }
+	Nunjucks.addFilter('fileName', string => {
+		return _.camelCase(string);
+	});
 
-    for (var i = 0; i < oneOfSchema.length; i++) {
-      let schema = oneOfSchema[i];
-      if (type !== '') {
-        type += '|';
-      }
-      if (oneOfSchema.length == i + 1) {
-        type += schema.uid()
-      } else {
-        type += schema.uid() + '|'
-      }
-    }
-    return type;
-  }
-  Nunjucks.addFilter('oneOfSchemaType', getTypeFromOneOf);
+	Nunjucks.addFilter('tsPayload', server => {
+		return 'STRING';
+	});
 
-  Nunjucks.addFilter('fileName', string => {
-    return _.camelCase(string);
-  });
+	Nunjucks.addFilter('tsEncoding', server => {
+		return 'STRING';
+	});
 
+	Nunjucks.addFilter('isPubsub', channel => {
+		let tempChannel = channel._json;
+		if (tempChannel.bindings.nats && tempChannel.bindings.nats.is == 'pubsub') {
+			return true;
+		}
+		return false;
+	});
 
-  Nunjucks.addFilter('tsPayload', (server) => {
-    return "STRING";
-  });
-  
-  Nunjucks.addFilter('tsEncoding', (server) => {
-    return "STRING";
-  });
+	Nunjucks.addFilter('hasNatsBindings', obj => {
+		if (obj.bindings.nats) {
+			return true;
+		}
+		return false;
+	});
 
+	Nunjucks.addFilter('isRequestReply', channel => {
+		let tempChannel = channel._json;
+		if (
+			tempChannel.bindings.nats &&
+			tempChannel.bindings.nats.is == 'requestReply'
+		) {
+			return true;
+		}
+		return false;
+	});
 
-  Nunjucks.addFilter('isPubsub', channel => {
-    let tempChannel = channel._json;
-    if (tempChannel.bindings.nats && tempChannel.bindings.nats.is == 'pubsub') {
-      return true;
-    }
-    return false;
-  });
+	Nunjucks.addFilter('isRequester', channel => {
+		let tempChannel = channel._json;
+		if (
+			tempChannel.bindings.nats &&
+			tempChannel.bindings.nats.is == 'requestReply' &&
+			tempChannel.bindings.nats.requestReply &&
+			tempChannel.bindings.nats.requestReply.is == 'requester'
+		) {
+			return true;
+		}
+		return false;
+	});
 
-  Nunjucks.addFilter('hasNatsBindings', obj => {
-    if (obj.bindings.nats) {
-      return true;
-    }
-    return false;
-  });
-
-  Nunjucks.addFilter('isRequestReply', channel => {
-    let tempChannel = channel._json;
-    if (tempChannel.bindings.nats && tempChannel.bindings.nats.is == 'requestReply') {
-      return true;
-    }
-    return false;
-  });
-
-  Nunjucks.addFilter('isRequester', channel => {
-    let tempChannel = channel._json;
-    if (tempChannel.bindings.nats && tempChannel.bindings.nats.is == 'requestReply' && tempChannel.bindings.nats.requestReply && tempChannel.bindings.nats.requestReply.is == 'requester') {
-      return true;
-    }
-    return false;
-  });
-
-  Nunjucks.addFilter('isReplier', channel => {
-    let tempChannel = channel._json;
-    if (tempChannel.bindings.nats && tempChannel.bindings.nats.is == 'requestReply' && tempChannel.bindings.nats.requestReply && tempChannel.bindings.nats.requestReply.is == 'replier') {
-      return true;
-    }
-    return false;
-  });
+	Nunjucks.addFilter('isReplier', channel => {
+		let tempChannel = channel._json;
+		if (
+			tempChannel.bindings.nats &&
+			tempChannel.bindings.nats.is == 'requestReply' &&
+			tempChannel.bindings.nats.requestReply &&
+			tempChannel.bindings.nats.requestReply.is == 'replier'
+		) {
+			return true;
+		}
+		return false;
+	});
 };
