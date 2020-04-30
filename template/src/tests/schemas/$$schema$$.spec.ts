0626ac0e4c08af8424c041f1df208c414b706411
jonaslagoni <jonas-lt@live.dk>
2020-04-30 12:13:41 +0200

    Removed $$schema$$.spec.ts

--- template/src/tests/schemas/$$schema$$.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import 'mocha';
-
-describe('First test', () => { 
-    it('should return true', () => { 
-      const result = helloTest();
-      expect(result).to.equal(true); 
-  }); 
-});
