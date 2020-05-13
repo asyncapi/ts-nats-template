c1b86cac04cb6fa65b281c3419f7c154ddf7f089
jonaslagoni <jonas-lt@live.dk>
2020-05-13 12:24:30 +0200

    Added  optional test files

---
+++ hooks/remove-test-files.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const Path = require('path');
+
+const deleteFolderRecursive = function(path) {
+	if (fs.existsSync(path) && path !== "/") {
+	  fs.readdirSync(path).forEach((file, index) => {
+		const curPath = Path.join(path, file);
+		if (fs.lstatSync(curPath).isDirectory()) { // recurse
+		  deleteFolderRecursive(curPath);
+		} else { // delete file
+		  fs.unlinkSync(curPath);
+		}
+	  });
+	  fs.rmdirSync(path);
+	}
+  };
+module.exports = register => {
+	register('generate:after', generator => {
+		if(generator.targetDir && generator.templateParams && !generator.templateParams.generateTestFiles){
+			pathToTests = Path.resolve(generator.targetDir, "src/tests/")
+			deleteFolderRecursive(pathToTests)
+		}
+	});
+};
