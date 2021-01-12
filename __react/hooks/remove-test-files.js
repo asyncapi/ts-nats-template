ba8b5bd2e6257ad248fe765b51005f0ac7fd89ba
Jonas Lagoni <jonas-lt@live.dk>
2021-01-12 12:52:47 +0100

    chore: Added initial React template setup (#21)
    * Added initial React template setup
    * Removed .DS_Store and added git ignore in main repository.

---
+++ __react/hooks/remove-test-files.js
@@ -0,0 +1,36 @@
+// detect-non-literal-fs-filename provides false-positives
+/* eslint-disable security/detect-non-literal-fs-filename */
+const fs = require('fs');
+const Path = require('path');
+
+/**
+ * Remove a folder recursively.
+ * 
+ * @param {string} path to recursively remove 
+ */
+const deleteFolderRecursive = function(path) {
+  if (fs.existsSync(path) && path !== '/') {
+    fs.readdirSync(path).forEach((file) => {
+      const curPath = Path.join(path, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+/**
+ * Since we cannot have a condition on a template whether to generate something 
+ * or not we have to remove the test client if its not specified. 
+ */
+module.exports = {
+  'generate:after': (generator) => {
+    if (generator.targetDir && generator.templateParams && !generator.templateParams.generateTestClient) {
+      const pathToTests = Path.resolve(generator.targetDir, 'src/tests/');
+      deleteFolderRecursive(pathToTests);
+    }
+  }
+};
