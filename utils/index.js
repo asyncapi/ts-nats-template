eba8e438a5c676060118f9b17441dc065e523dd1
Jonas Lagoni <jonas-lt@live.dk>
2021-01-14 18:52:04 +0100

    Fixed functions where not exported

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
-import { camelCase, pascalCase, kebabCase, isBinaryPayload, isStringPayload, isJsonPayload, messageHasNotNullPayload, getMessageType, containsBinaryPayload, containsStringPayload, containsJsonPayload, toTsType, castToTsType, getClientToUse, shouldPromisfyCallbacks} from './general';
-export { camelCase, pascalCase, kebabCase, isBinaryPayload, isStringPayload, isJsonPayload, messageHasNotNullPayload, getMessageType, containsBinaryPayload, containsStringPayload, containsJsonPayload, toTsType, castToTsType, getClientToUse, shouldPromisfyCallbacks};
+import { camelCase, pascalCase, kebabCase, isBinaryPayload, isStringPayload, isJsonPayload, messageHasNotNullPayload, getMessageType, containsBinaryPayload, containsStringPayload, containsJsonPayload, toTsType, castToTsType, getClientToUse, shouldPromisifyCallbacks} from './general';
+export { camelCase, pascalCase, kebabCase, isBinaryPayload, isStringPayload, isJsonPayload, messageHasNotNullPayload, getMessageType, containsBinaryPayload, containsStringPayload, containsJsonPayload, toTsType, castToTsType, getClientToUse, shouldPromisifyCallbacks};
 
 import { hasNatsBindings, isPubsub, isRequester, isRequestReply, isReplier, includeQueueForSubscription, includeUnsubAfterForSubscription } from './bindings';
 export { hasNatsBindings, isPubsub, isRequester, isRequestReply, isReplier, includeQueueForSubscription, includeUnsubAfterForSubscription };
@@ -7,5 +7,5 @@ export { hasNatsBindings, isPubsub, isRequester, isRequestReply, isReplier, incl
 import { realizeChannelNameWithoutParameters, realizeChannelName } from './channelname';
 export { realizeChannelNameWithoutParameters, realizeChannelName };
 
-import { realizeParametersForChannelWithoutType, realizeParametersForChannelWrapper, realizeParametersForChannel, realizeParameterForChannelWithoutType } from './parameters';
-export { realizeParametersForChannelWithoutType, realizeParametersForChannelWrapper, realizeParametersForChannel, realizeParameterForChannelWithoutType };
+import { realizeParametersForChannelWithoutType, realizeParametersForChannelWrapper, realizeParametersForChannel } from './parameters';
+export { realizeParametersForChannelWithoutType, realizeParametersForChannelWrapper, realizeParametersForChannel };
