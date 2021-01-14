cf48907e8cf6a14d144167d9bcf533cde54541dc
Jonas Lagoni <jonas-lt@live.dk>
2021-01-14 11:49:42 +0100

    chore: Refactored test channels to React (#25)
    * Added test channel
    * Refactored channel code to reduce duplicate code.

--- __react/template/src/tests/testclient/testchannels/$$channel$$.ts.js
+++ __react/template/src/tests/testclient/testchannels/$$channel$$.ts.js
@@ -1,73 +1,71 @@
 import { File } from '@asyncapi/generator-react-sdk';
-import { Publish } from '../../../components/channel/publish';
-import { Subscribe } from '../../../components/channel/subscribe';
-import { Reply } from '../../../components/channel/reply';
-import { Request } from '../../../components/channel/request';
-import { General } from '../../../components/channel/general';
-import { pascalCase, isRequestReply, isReplier, isRequester, isPubsub, messageHasNotNullPayload} from '../../../utils/index';
+import { Publish } from '../../../../../components/channel/publish';
+import { Subscribe } from '../../../../../components/channel/subscribe';
+import { Reply } from '../../../../../components/channel/reply';
+import { Request } from '../../../../../components/channel/request';
+import { pascalCase, isRequestReply, isReplier, isRequester, isPubsub, messageHasNotNullPayload} from '../../../../../utils/index';
 
 /**
  * Return the correct channel component based on whether its `pubSub` or `requestReply`.
  * 
+ * NOTICE this is a reverse of the normal client channel.
+ * 
  * @param {*} asyncapi 
- * @param {*} channel to determine the type of
+ * @param {*} channel 
  * @param {*} channelName 
- * @param {*} params
+ * @param {*} params 
  */
 function getChannelCode(asyncapi, channel, channelName, params) {
-  const publishMessage = channel.publish().message(0);
-  const subscribeMessage = channel.subscribe().message(0);
   let channelcode;
   if (isRequestReply(channel)) {
     if (isRequester(channel)) {
-      channelcode = Request(
-        asyncapi.defaultContentType(), 
-        channelName, 
-        subscribeMessage,
-        publishMessage,
-        channel.parameters()
-      );
-    }
-    if (isReplier(channel)) {
       channelcode = Reply(
         asyncapi.defaultContentType(), 
         channelName, 
-        subscribeMessage,
-        publishMessage,
+        channel.publish().message(0),
+        channel.subscribe().message(0),
         channel.parameters(),
         params
+      );
+    }
+    if (isReplier(channel)) {
+      channelcode = Request(
+        asyncapi.defaultContentType(), 
+        channelName, 
+        channel.publish().message(0),
+        channel.subscribe().message(0),
+        channel.parameters()
       );
     }
   }
 
   if (isPubsub(channel)) {
     if (channel.hasSubscribe()) {
-      channelcode = Publish(
-        asyncapi.defaultContentType(), 
-        channelName, 
-        subscribeMessage, 
-        channel.parameters());
-    }
-    if (channel.hasPublish()) {
       channelcode = Subscribe(
         asyncapi.defaultContentType(), 
         channelName, 
-        publishMessage, 
+        channel.subscribe().message(0),
+        channel.parameters());
+    }
+    if (channel.hasPublish()) {
+      channelcode = Publish(
+        asyncapi.defaultContentType(), 
+        channelName, 
+        channel.publish().message(0), 
         channel.parameters());
     }
   }
   return channelcode;
 }
-
 export default function channelRender({ asyncapi, channelName, channel, params }) {
   const publishMessage = channel.publish().message(0);
   const subscribeMessage = channel.subscribe().message(0);
 
   return <File name={`${pascalCase(channelName)}.ts`}>
-    {`
-${General(channel, publishMessage, subscribeMessage, '..')}
+{`
+${General(channel, publishMessage, subscribeMessage, '../../..')}
 
 ${getChannelCode(asyncapi, channel, channelName, params)}
-    `}
+`}
   </File>;
 }
