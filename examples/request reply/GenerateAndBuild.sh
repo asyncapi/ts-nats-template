045c6c764063e3ce0ead028288764e9c2000f836
derberg <lpgornicki@gmail.com>
2021-01-27 20:50:14 +0100

    end

---
+++ examples/request reply/GenerateAndBuild.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ag --output "./streetlight" "./streetlight.json" "../../" --force-write --param "generateTestClient=true" --param "promisifyReplyCallback=true" && cd ./streetlight && npm i && npm run build && cd ..
