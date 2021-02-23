f70ea62bae15b97098f9206db0e0ed07c2005f09
Jonas Lagoni <jonas-lt@live.dk>
2021-02-23 12:48:36 +0100

    Updated code

--- hooks/quicktype-schema.js
+++ hooks/quicktype-schema.js
@@ -1,18 +1,34 @@
-const quicktypeFilter = require('@lagoni/asyncapi-quicktype-filter');
+const {TypeScriptGenerator} = require('@asyncapi/generator-model-sdk');
+const fs = require('fs');
+const Path = require('path');
+
+const _ = require('lodash');
+function pascalCase(string) {
+  string = _.camelCase(string);
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 /**
  * Use quicktype to generate messages with their payload.
  */
 module.exports = {
   'generate:after': async (generator) => {
-    const allMessages = generator.asyncapi.allMessages();
-    await quicktypeFilter.generateAllMessagePayloads(
-      generator.targetDir,
-      {
-        quicktypeLanguage: 'typescript',
-        subTargetDir: 'src/messages/'
-      },
-      allMessages
-    );
+    const typescriptGenerator = new TypeScriptGenerator();
+    const generatedModels = await typescriptGenerator.generate(generator.asyncapi);
+    for (const generatedModel of generatedModels) {
+      
+      const targetDir = Path.join(generator.targetDir, 'src/messages/');
+      await fs.promises
+        .mkdir(targetDir, { recursive: true })
+        .catch(console.error);
+      fs.mkdirSync(targetDir, { recursive: true });
+      fs.writeFileSync(
+        Path.join(
+          targetDir,
+          `${pascalCase(generatedModel.modelName)}.ts`
+        ),
+        generatedModel.result
+      );
+    }
   }
 };
