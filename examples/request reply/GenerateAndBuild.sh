6761bbcad4762d32c7d865304ba600efba83db0e
Jonas Lagoni <jonas-lt@live.dk>
2021-03-03 17:18:39 +0100

    Fixed issue where test client is not found

--- examples/request reply/GenerateAndBuild.sh
+++ examples/request reply/GenerateAndBuild.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+rm -rf ./streetlight
 ../../node_modules/@asyncapi/generator/cli.js --output "./streetlight" "./streetlight.json" "../../" --force-write --param "generateTestClient=true" --param "promisifyReplyCallback=true" && cd ./streetlight && npm i && npm run build && cd ..
