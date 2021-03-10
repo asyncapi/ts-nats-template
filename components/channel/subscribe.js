51a724c96e76c116d90582857c834e9307cef260
Jonas Lagoni <jonas-lt@live.dk>
2021-03-10 10:07:32 +0100

    Update components/channel/subscribe.js
    Co-authored-by: Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>

--- components/channel/subscribe.js
+++ components/channel/subscribe.js
@@ -36,8 +36,7 @@ export function Subscribe(defaultContentType, channelName, message, channelParam
   return `
   
   /**
-   * Internal functionality to setup subscription on channel 
-   * ${channelName}
+   * Internal functionality to setup subscription on the `${channelName}` channel 
    * 
    * @param onDataCallback to call when messages are received
    * @param client to subscribe with
