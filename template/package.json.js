25cd525bfa1eecad02b8b9aed8b210ce73ed43cf
Jonas Lagoni <jonas-lt@live.dk>
2021-11-29 09:55:37 +0100

    fix: audit dependencies (#387)
    * Fix audit dependencies
    * Fixed template scripts not working for some Windows versions

--- template/package.json.js
+++ template/package.json.js
@@ -12,9 +12,9 @@ export default function packageFile({ asyncapi }) {
     scripts: {
       watch: 'tsc --watch',
       build: 'tsc && npm run docs',
-      'test:integration': './node_modules/.bin/mocha -r ts-node/register tests/integration/**/*.spec.ts --exit --timeout 10000',
+      'test:integration': 'mocha -r ts-node/register tests/integration/**/*.spec.ts --exit --timeout 10000',
       docs: 'npm run docs:markdown',
-      'docs:markdown': './node_modules/.bin/jsdoc2md lib/index.js -f lib/**/*.js > API.md'
+      'docs:markdown': 'jsdoc2md lib/index.js -f lib/**/*.js > API.md'
     },
     dependencies: {
       '@types/klaw-sync': '^6.0.0',
