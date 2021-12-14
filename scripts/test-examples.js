458ab24e4faf7a58b88175935fbda246e1a5b3d8
Jonas Lagoni <jonas-lt@live.dk>
2021-12-14 16:15:31 +0100

    Fixed windows could not execute example script

---
+++ scripts/test-examples.js
@@ -0,0 +1,19 @@
+/**
+ * Small script that tests the examples.
+ * 
+ * This script is necessary to provide cross platform support with windows.
+ */
+
+const fs = require('fs');
+const path = require('path');
+const examplePath = path.join(__dirname, '../examples/');
+// eslint-disable-next-line security/detect-child-process
+const { execSync } = require('child_process');
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+fs.readdirSync(examplePath)
+  .map((file) => {return path.resolve(examplePath, file);})
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  .filter((exampleDir) => {return fs.lstatSync(exampleDir).isDirectory();})
+  .forEach((exampleDir) => {
+    execSync(`npm run test --prefix ${exampleDir}`);
+  });
