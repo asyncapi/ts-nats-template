9568325ced519dcd6ea5f8a3b2aa8fb2d63ed7a7
Jonas Lagoni <jonas-lt@live.dk>
2021-12-14 17:19:37 +0100

    Fixed path was not being resolved for windows environment

--- scripts/test-examples.js
+++ scripts/test-examples.js
@@ -3,7 +3,10 @@
  * 
  * This script is necessary to provide cross platform support with windows.
  */
+
+const fs = require('fs');
 const path = require('path');
+const examplePath = path.resolve(__dirname, '..', 'examples');
 // eslint-disable-next-line security/detect-child-process
 const { execSync } = require('child_process');
 
@@ -12,10 +15,18 @@ const { execSync } = require('child_process');
 // 'darwin' on OSX
 const os = require('os');
 const platform = os.platform();
-let command = 'test';
-if (platform === 'win32') {
-  command += ':windows';
-}
 
-const examplePath = path.resolve(__dirname, '..', 'examples');
-execSync(`find ${examplePath} -not -iwholename '${examplePath}' -maxdepth 1 -type d -exec npm run ${command} --prefix {} \\;`, {stdio: 'inherit'});
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+fs.readdirSync(examplePath)
+  .map((file) => {return path.resolve(examplePath, file);})
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  .filter((exampleDir) => {return fs.lstatSync(exampleDir).isDirectory();})
+  .forEach((exampleDir) => {
+    let command = 'test';
+    if (platform === 'win32') {
+      command += ':windows';
+    }
+    execSync(`npm run ${command} --prefix ${exampleDir}`, {stdio: 'inherit'});
+  });
+
+execSync(`npm run ${command} --prefix ${exampleDir}`);
