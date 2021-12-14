46ae17fd4818d386f1d5fbacd4891459c200a536
Jonas Lagoni <jonas-lt@live.dk>
2021-12-14 16:47:58 +0100

    Fixed path was not being resolved for windows environment

--- scripts/test-examples.js
+++ scripts/test-examples.js
@@ -9,11 +9,22 @@ const path = require('path');
 const examplePath = path.resolve(__dirname, '..', 'examples');
 // eslint-disable-next-line security/detect-child-process
 const { execSync } = require('child_process');
+
+// 'linux' on Linux
+// 'win32' on Windows (32-bit / 64-bit)
+// 'darwin' on OSX
+const os = require('os');
+const platform = os.platform();
+
 // eslint-disable-next-line security/detect-non-literal-fs-filename
 fs.readdirSync(examplePath)
   .map((file) => {return path.resolve(examplePath, file);})
   // eslint-disable-next-line security/detect-non-literal-fs-filename
   .filter((exampleDir) => {return fs.lstatSync(exampleDir).isDirectory();})
   .forEach((exampleDir) => {
-    execSync(`npm run test --prefix ${exampleDir}`);
+    let command = 'test';
+    if (platform === 'win32') {
+      command += ':windows';
+    }
+    execSync(`npm run ${command} --prefix ${exampleDir}`);
   });
