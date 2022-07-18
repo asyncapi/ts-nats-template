9dbbc72183b0b022147bf7e2a6d689f61fd2f571
Jonas Lagoni <jonas-lt@live.dk>
2022-07-19 01:36:33 +0200

    add pull components

---
+++ components/index/pull.js
@@ -0,0 +1,36 @@
+import { camelCase, getMessageType, realizeParametersForChannelWrapper, renderJSDocParameters, realizeParametersForChannelWithoutType, pascalCase} from '../../utils/index';
+
+export function Pull(channelName, message, messageDescription, channelParameters) {
+  return `
+  /**
+    * JetStream pull function.
+    * 
+    * Pull message from \`${channelName}\`
+    * 
+    * ${messageDescription}
+    * 
+    * @param onDataCallback to call when messages are received
+    ${renderJSDocParameters(channelParameters)}
+    * @param options to pull message with, bindings from the AsyncAPI document overwrite these if specified
+    */
+    public jetStreamPull${pascalCase(channelName)}(
+      onDataCallback: (
+        err ? : NatsTypescriptTemplateError,
+        msg?: ${getMessageType(message)}
+        ${realizeParametersForChannelWrapper(channelParameters, false)}
+        jetstreamMsg?: Nats.JsMsg) => void,
+      ${realizeParametersForChannelWrapper(channelParameters)},
+    ): void {
+      if (!this.isClosed() && this.nc !== undefined && this.codec !== undefined && this.js !== undefined) {
+        ${camelCase(channelName)}Channel.jetsStreamPull(
+          onDataCallback,
+          this.js,
+          this.codec 
+          ${Object.keys(channelParameters).length ? ` ,${realizeParametersForChannelWithoutType(channelParameters)},` : ''}
+        );
+      } else {
+        throw NatsTypescriptTemplateError.errorForCode(ErrorCode.NOT_CONNECTED);
+      }
+    }
+    `;
+}
