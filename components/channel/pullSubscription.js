661d7d5443380b9243fc52f883f7a6934bf79765
Jonas Lagoni <jonas-lt@live.dk>
2022-07-19 01:37:47 +0200

    add components

---
+++ components/channel/pullSubscription.js
@@ -0,0 +1,79 @@
+import { realizeChannelName, camelCase, getMessageType, includeUnsubAfterForSubscription, messageHasNullPayload, realizeParametersForChannelWrapper, includeQueueForSubscription, renderJSDocParameters} from '../../utils/index';
+import { unwrap } from './ChannelParameterUnwrap';
+// eslint-disable-next-line no-unused-vars
+import { Message, ChannelParameter } from '@asyncapi/parser';
+
+/**
+ * Component which returns a function which subscribes to the given channel
+ * 
+ * @param {string} defaultContentType 
+ * @param {string} channelName to subscribe to
+ * @param {Message} message which is being received
+ * @param {Object.<string, ChannelParameter>} channelParameters parameters to the channel
+ */
+export function PullSubscription(channelName, message, channelParameters, operation) {
+  const messageType = getMessageType(message);
+  let parameters = [];
+  parameters = Object.entries(channelParameters).map(([parameterName]) => {
+    return `${camelCase(parameterName)}Param`;
+  });
+  const hasNullPayload = messageHasNullPayload(message.payload());
+
+  //Determine the callback process when receiving messages.
+  //If the message payload is null no hooks are called to process the received data.
+  let whenReceivingMessage = `onDataCallback(undefined, null ${parameters.length > 0 && `, ${parameters.join(',')}`});`;
+  if (!hasNullPayload) {
+    whenReceivingMessage =  `
+    let receivedData: any = codec.decode(msg.data);
+    onDataCallback(undefined, ${messageType}.unmarshal(receivedData) ${parameters.length > 0 && `, ${parameters.join(',')}`});
+    `;
+  }
+  
+  return `
+  
+  /**
+   * Internal functionality to setup jetstream pull subscription on the \`${channelName}\` channel 
+   * 
+   * @param onDataCallback to call when messages are received
+   * @param nc to subscribe with
+   * @param codec used to convert messages
+   ${renderJSDocParameters(channelParameters)}
+   */
+  export function jetStreamPullSubscribe(
+    onDataCallback: (
+      err ? : NatsTypescriptTemplateError,
+      msg?: ${messageType}
+      ${realizeParametersForChannelWrapper(channelParameters, false)}) => void,
+      jetstreamMsg?: Nats.JsMsg) => void,
+    js: Nats.JetStreamClient,
+    codec: Nats.Codec < any > 
+    ${realizeParametersForChannelWrapper(channelParameters)}
+  ): Promise < Nats.JetStreamPullSubscription > {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const subscription = await js.pullSubscribe(${realizeChannelName(channelParameters, channelName)}, {
+          mack: true,
+          // artificially low ack_wait, to show some messages
+          // not getting acked being redelivered
+          config: {
+            durable_name: 'durable',
+            ack_policy: AckPolicy.Explicit,
+            ack_wait: nanos(4000),
+          },
+        });
+  
+        (async () => {
+          for await (const msg of subscription) {
+            ${unwrap(channelName, channelParameters)}
+
+            ${whenReceivingMessage}
+          }
+        })();
+        resolve(subscription);
+      } catch (e: any) {
+        reject(NatsTypescriptTemplateError.errorForCode(ErrorCode.INTERNAL_NATS_TS_ERROR, e));
+      }
+    })
+  }
+    `;
+}
